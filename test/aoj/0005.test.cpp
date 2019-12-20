333177f402b2bad6862a8b5218a61c27de3ab6fd
sash <sash2104@gmail.com>
2019-12-21 00:38:25 +0900

    :tada: ユークリッドの互除法でGCDとLCM求めるテンプレートを追加

---
+++ test/aoj/0005.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0005"
+
+#define call_from_test
+#include "../../math/euclidean-algorithm.cpp"
+#undef call_from_test
+
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+
+int main() {
+  ll a, b;
+  cin >> a >> b;
+  cout << gcd(a, b) << " " << lcm(a, b) << endl;
+  return 0;
+}
