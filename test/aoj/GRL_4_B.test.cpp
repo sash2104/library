48b6fb70d40f5b82caa7f82d7b2cc4edd20fc5d3
sash <sash2104@gmail.com>
2019-12-22 11:37:37 +0900

    :tada: add topological sort

---
+++ test/aoj/GRL_4_B.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/GRL_4_B"
+#include "../../graph/topological-sort.cpp"
+
+#include <iostream>
+using namespace std;
+
+using G = Graph<int>;
+int main() {
+  int V, E; cin >> V >> E;
+  G graph(V);
+  for (int i = 0; i < E; ++i) {
+    int s, t; cin >> s >> t;
+    graph[s].emplace_back(s, t);
+  }
+  vector<int> order = topologicalSort(graph);
+  for (int v: order) { cout << v << endl; }
+}
