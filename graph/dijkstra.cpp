d4dbbff363a8280eaf3a54c0e1d9f28bb1d51e4c
sash <sash2104@gmail.com>
2020-01-02 01:18:54 +0900

    :tada: add dijkstra

---
+++ graph/dijkstra.cpp
@@ -0,0 +1,40 @@
+#include "template.cpp"
+
+#include <algorithm>
+#include <functional>
+#include <limits>
+#include <queue>
+#include <utility>
+#include <vector>
+
+template < typename T >
+struct Dijkstra {
+  using P = std::pair<T, int>; // <始点からの距離, 点のid>
+  const T INF; // 十分に大きいがoverflowしない値として、型Tの最大値の半分を使う
+  const int V; // 頂点数
+  Graph<T> g;
+  std::vector<T> dist; // 始点からの距離
+  std::vector<bool> visit; // すでに探索済みの点か
+  Dijkstra(Graph<T> &g): INF(std::numeric_limits<T>::max()/2), V(g.size()), g(g), dist(V), visit(V) {}
+  Dijkstra(Graph<T> &g, int V): INF(std::numeric_limits<T>::max()/2), V(V), g(g), dist(V), visit(V) {}
+  void init(int s) {
+    std::fill(dist.begin(), dist.end(), INF);
+    std::fill(visit.begin(), visit.end(), false);
+    dist[s] = T(); // 始点の距離を0で初期化
+    std::priority_queue<P, std::vector<P>, std::greater<P>> pq;
+    pq.push({dist[s], s});
+    while (!pq.empty()) {
+      P node = pq.top(); pq.pop();
+      int from = node.second;
+      if (visit[from]) continue;
+      visit[from] = true;
+      for (auto &e : g[from]) {
+        if (visit[e.to]) continue;
+        T d = node.first + e.cost;
+        if (d >= dist[e.to]) continue;
+        dist[e.to] = d;
+        pq.push({d, e.to});
+      }
+    }
+  }
+};
