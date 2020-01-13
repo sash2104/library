cd634aac223334301aad63263d8e08de6cfe3360
sash <sash2104@gmail.com>
2020-01-13 21:06:50 +0900

    :tada: add geometry template

---
+++ test/aoj/CGL_1_A.test.cpp
@@ -0,0 +1,14 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=CGL_1_A"
+#include "../../geometry/template.hpp"
+
+#include <iostream>
+using namespace std;
+int main() {
+  Line l; cin >> l;
+  int q; cin >> q;
+  for (int i = 0; i < q; ++i) {
+    Point p; cin >> p;
+    Point x = projection(l, p);
+    cout << x << endl;
+  }
+}
