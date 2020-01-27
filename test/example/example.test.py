d815537792e345d040216ded40b57295b1eaa7a9
sash <sash2104@gmail.com>
2020-01-27 23:14:51 +0900

    :bug: fix typo

--- test/example/example.test.py
+++ test/example/example.test.py
@@ -1,4 +1,4 @@
-# verify-heler : PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A
+# verify-helper : PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/1/ITP1_1_A
 
 if __name__ == "__main__":
     print("Hello World")
