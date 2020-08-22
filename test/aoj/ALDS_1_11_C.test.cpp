e479035e0877da0719ad466915f22cad663f0e82
sash <sash2104@gmail.com>
2020-08-22 23:18:55 +0900

    :tada: add 01-bfs

---
+++ test/aoj/ALDS_1_11_C.test.cpp
@@ -0,0 +1,25 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_11_C"
+
+#include "../../graph/bin-bfs.cpp"
+#include <iostream>
+using namespace std; 
+
+int main() {
+  int n; cin >> n;
+  Graph<int> g(n);
+  for (int i = 0; i < n; ++i) {
+    int u, k; cin >> u >> k;
+    --u;
+    for (int j = 0; j < k; ++j) {
+      int v; cin >> v; --v;
+      g[u].emplace_back(u, v, 1);
+    }
+  }
+  BinBFS<int> bfs(g);
+  bfs.init(0);
+  for (int i = 0; i < n; ++i) {
+    int d = bfs.dist[i];
+    if (d == bfs.INF) d = -1;
+    cout << i+1 << " " << d << endl;
+  }
+}
