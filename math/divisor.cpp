868d9a74034db6036bee5e4d846b663b8f8f757b
sash <sash2104@gmail.com>
2020-08-30 21:16:58 +0900

    :recycle: divisor

--- math/divisor.cpp
+++ math/divisor.cpp
@@ -1,20 +1,17 @@
+// @title 約数列挙
 #include <algorithm>
-#include <iostream>
 #include <vector>
-#include <map>
 
-using namespace std;
+using ll = long long;
 
-typedef long long ll;
-
-vector<ll> divisor(ll n) { 
-  vector<ll> ret;
+std::vector<ll> divisor(ll n) { 
+  std::vector<ll> ret;
   for (ll i = 1; (ll)i*i <= n; ++i) { 
     if (n % i == 0) {
       ret.push_back(i);
       if (i*i != n) { ret.push_back(n/i); }
     }
   }
-  sort(ret.begin(), ret.end());
+  std::sort(ret.begin(), ret.end());
   return ret;
 };
