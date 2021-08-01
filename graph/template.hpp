77f8078e67a3a64bb6da6710190c1ed0548acbaa
sash <sash2104@gmail.com>
2021-08-01 23:46:19 +0900

    :+1: graphのdebug printを追加

--- graph/template.hpp
+++ graph/template.hpp
@@ -1,6 +1,7 @@
 #ifndef __GRAPH__TEMPLATE
 #define __GRAPH__TEMPLATE
 
+#include <iostream>
 #include <vector>
 
 template< typename T >
@@ -20,5 +21,17 @@ template< typename T >
 using Edges = std::vector< Edge< T > >;
 template< typename T >
 using Graph = std::vector< Edges< T > >;
+
+template< typename T >
+void debug(Graph<T> &g, int n = -1) {
+    if (n == -1) n = g.size();
+    for (int i = 0; i < n; ++i) {
+        std::cerr << i  << "\t";
+        for (auto &e: g[i]) {
+            std::cerr << e.to << ",";
+        }
+        std::cerr << std::endl;
+    }
+}
 
 #endif // __GRAPH_TEMPLATE
